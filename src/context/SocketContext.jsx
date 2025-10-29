b7b0f7e4a6730c1497624d177d20723349f2fdd4
GiOrGI175 <nozadzegiorgi1011@gmail.com>
2025-10-30 03:21:13 +0400

    fix/api

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -10,7 +10,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io('http://localhost:8800'));
+    setSocket(io('https://lamaesate-back.onrender.com'));
   }, []);
 
   useEffect(() => {
